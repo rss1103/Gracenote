56aface827186eb19d035affd05c449e9805722c
Rohan Shiroor <rss1103@rit.edu>
2019-11-19 11:59:13 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServerService } from './server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Gracenote';
+  events: any[] = [];
+  constructor(private server: ServerService) { }
+  ngOnInit() {
+    this.getEvents();
+  }
+  private getEvents() {
+    this.server.getEvents().then((response: any) => {
+      // console.log(response.data.toJSON());
+      this.events = response.data;
+    });
+  }
+  // castThree(cast: Array<Object>): any[] {
+  //   console.log(cast.slice(3));
+  //   return cast.slice(3);
+  // }
 }
